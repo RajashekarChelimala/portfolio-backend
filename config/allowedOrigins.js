c15e8de062f028f5e048037e6ca0a875f73a5f8c
RajashekarChelimala <rajashekarchelimela1@gmail.com>
2024-09-01 17:44:27 +0530

    Admin Production Allow Origin Addition

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://rajashekar-chelimala.vercel.app'
+    'https://rajashekar-chelimala.vercel.app',
+    'https://rajashekar-chelimala-admin.vercel.app',
 ];
 
 export default allowedOrigins;
